3495fc49c5446d0ac510de338f0dd53460793c0f
shyam <007shyamagarwal@gmail.com>
2018-06-15 13:09:40 +0400

    parsed request query and then response

--- hello-world.js
+++ hello-world.js
@@ -1,6 +1,32 @@
-const express = require('express')
-const app = express()
+const http = require('http');
+const url = require('url');
+const responseCreator = require('./responseCreator');
+const convertQueryStringToJSON = require('./queryParameterProcessor');
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const people = require('./data.json');
+console.log("people 0" , people[0]);
+const peopleSchema = Object.keys(people[0]);
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+
+http.createServer(function (request, response) {
+  const {pathname,query} = url.parse(request.url);
+  const queryParameter = convertQueryStringToJSON(query);
+  //check for the non people
+  if(pathname !== '/people'){
+    response = responseCreator(response, 404, []);
+    response.end();
+  }
+  else{
+    //means we got people
+    const keysToQuery = Object.keys(queryParameter);
+    let filteredData = people;
+    keysToQuery.forEach((queryField) => {
+      filteredData = peopleSchema.indexOf(queryField) === -1 ? 
+      filteredData : filteredData.filter((data) => data[queryField].includes(queryParameter[queryField]));
+      return filteredData;
+    })
+    responseCreator(response, 200 , filteredData);
+    response.end();
+  }
+
+}).listen(7000);
